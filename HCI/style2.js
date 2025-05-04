44c894ed096020a3f354a0f85919ca520223d41c
rrypogi143 <rryyibanez@gmail.com>
2025-05-04 03:22:39 -0700

    Add files via upload

---
+++ HCI/style2.js
@@ -0,0 +1,10 @@
+// Hero booking button
+document.querySelector('.hero-book-btn')?.addEventListener('click', () => {
+  alert('Redirecting you to the booking page!');
+  // Example: window.location.href = "book-table.html";
+});
+
+// Subscribe button
+document.querySelector('.subscribe-btn')?.addEventListener('click', () => {
+  alert('Subscribed to the newsletter!');
+});
